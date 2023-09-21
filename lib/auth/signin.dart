a52773302563f487a03dbb778071d8858b0bf94e
aa-mahmud <abdullahmahmud690702@gmail.com>
2023-09-21 14:08:53 +0600

    done

--- lib/auth/signin.dart
+++ lib/auth/signin.dart
@@ -23,97 +23,100 @@ class _SignInState extends State<SignIn> {
         centerTitle: true,
         automaticallyImplyLeading: false,
       ),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.only(left: 65, right: 65),
-          child: Form(
-            key: AuthValid.formkey,
-            child: Column(
-              children: [
-                Container(
-                    height: 150,
-                    width: 150,
-                    child: Image.asset('assets/02.jpg')),
-                SizedBox(
-                  height: 10,
-                ),
-                Text(
-                  'WELCOME',
-                  style: TextStyle(
-                      fontSize: 30,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.cyan),
-                ),
-                TextFormField(
-                  validator: (value) {
-                    if (value == "" || value == null) {
-                      return "Data is Wrong";
-                    }
-                  },
-                  decoration: InputDecoration(
-                    hintText: "User Name or Email",
-                    label: Text("Email"),
-                    prefixIcon: Icon(Icons.person),
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(50)),
-                        borderSide: BorderSide(width: 3)),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.only(left: 65, right: 65),
+            child: Form(
+              key: AuthValid.formkey,
+              child: Column(
+                children: [
+                  Container(
+                      height: 150,
+                      width: 150,
+                      child: Image.asset('assets/02.jpg')),
+                  SizedBox(
+                    height: 10,
                   ),
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                TextFormField(
-                  validator: (value) {
-                    if (value == "" || value == null) {
-                      return "Data is Wrong";
-                    }
-                  },
-                  obscureText: obsSecure,
-                  decoration: InputDecoration(
-                    hintText: "Password",
-                    label: Text("Password"),
-                    prefixIcon: Icon(Icons.lock),
-                    suffixIcon: GestureDetector(
-                      onTap: () {
-                        obsSecure = !obsSecure;
-                        setState(() {});
-                      },
-                      child: obsSecure == false
-                          ? Icon(Icons.visibility)
-                          : Icon(Icons.visibility_off),
-                    ),
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(50)),
-                        borderSide: BorderSide(width: 3)),
+                  Text(
+                    'WELCOME',
+                    style: TextStyle(
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.cyan),
                   ),
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    TextButton(onPressed: () {}, child: Text('Forget Password'))
-                  ],
-                ),
-                ElevatedButton(
-                    onPressed: () {
-                      AuthValid.formkey.currentState?.validate();
+                  TextFormField(
+                    validator: (value) {
+                      if (value == "" || value == null) {
+                        return "Data is Wrong";
+                      }
                     },
-                    child: Text(
-                      'Sign in',
-                      style: TextStyle(
-                        fontSize: 20,
+                    decoration: InputDecoration(
+                      hintText: "User Name or Email",
+                      label: Text("Email"),
+                      prefixIcon: Icon(Icons.person),
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(50)),
+                          borderSide: BorderSide(width: 3)),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  TextFormField(
+                    validator: (value) {
+                      if (value == "" || value == null) {
+                        return "Data is Wrong";
+                      }
+                    },
+                    obscureText: obsSecure,
+                    decoration: InputDecoration(
+                      hintText: "Password",
+                      label: Text("Password"),
+                      prefixIcon: Icon(Icons.lock),
+                      suffixIcon: GestureDetector(
+                        onTap: () {
+                          obsSecure = !obsSecure;
+                          setState(() {});
+                        },
+                        child: obsSecure == false
+                            ? Icon(Icons.visibility)
+                            : Icon(Icons.visibility_off),
                       ),
-                    )),
-                SizedBox(
-                  height: 20,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text("Dont have an Account?"),
-                    TextButton(onPressed: () {}, child: Text("Sign Up")),
-                  ],
-                ),
-              ],
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(50)),
+                          borderSide: BorderSide(width: 3)),
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      TextButton(
+                          onPressed: () {}, child: Text('Forget Password'))
+                    ],
+                  ),
+                  ElevatedButton(
+                      onPressed: () {
+                        AuthValid.formkey.currentState?.validate();
+                      },
+                      child: Text(
+                        'Sign in',
+                        style: TextStyle(
+                          fontSize: 20,
+                        ),
+                      )),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text("Dont have an Account?"),
+                      TextButton(onPressed: () {}, child: Text("Sign Up")),
+                    ],
+                  ),
+                ],
+              ),
             ),
           ),
         ),
